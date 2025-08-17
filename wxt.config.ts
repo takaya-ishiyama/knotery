ac616709fa1217941069753b6a3550e3882b73bd
takaya-ishiyama <takaya_ishiyama0810@yahoo.co.jp>
2025-08-17 22:21:23 +0900

    icon設定

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-solid'],
+	modules: ["@wxt-dev/module-solid"],
+	manifest: {
+		icons: {
+			16: "icon/icon16.png",
+			48: "icon/icon48.png",
+			128: "icon/icon128.png",
+		},
+	},
 });
